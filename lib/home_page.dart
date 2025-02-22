aa5e85745ddb6b512ca311ecc00978cf36c25812
Jatin Karthik Tripathy <jatinkarthikt@gmail.com>
2025-02-22 14:16:33 +0100

    feat: we loggin in baby
    squash

---
+++ lib/home_page.dart
@@ -0,0 +1,104 @@
+import 'package:flutter/material.dart';
+import 'package:secret_keeper/add_password_screen.dart';
+import 'package:secret_keeper/database_helper.dart';
+import 'package:secret_keeper/password.dart';
+import 'package:secret_keeper/password_search_delegate.dart';
+import 'package:secret_keeper/show_password_screen.dart';
+
+class HomePage extends StatefulWidget {
+  final String masterPassword;
+  const HomePage({super.key, required this.masterPassword});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  late Future<List<Password>> passwordsFuture;
+
+  @override
+  void initState() {
+    super.initState();
+    passwordsFuture = DatabaseHelper().getPasswords();
+  }
+
+  void _refreshPasswords() {
+    setState(() {
+      passwordsFuture = DatabaseHelper().getPasswords();
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.deepPurple[100],
+      appBar: AppBar(
+        backgroundColor: Colors.deepPurple[100],
+        title: Text('Secret Keeper'),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.search_rounded, color: Colors.deepPurple),
+            onPressed: () async {
+              final list = await DatabaseHelper().getPasswords();
+              if (!mounted) return;
+              showSearch(
+                context: context,
+                delegate: PasswordSearchDelegate(passwords: list),
+              );
+            },
+          ),
+        ],
+      ),
+      body: FutureBuilder<List<Password>>(
+        future: passwordsFuture,
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Center(child: CircularProgressIndicator());
+          } else if (snapshot.hasError) {
+            return Center(child: Text('Error: ${snapshot.error}'));
+          } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
+            return Center(child: Text('No passwords found.'));
+          } else {
+            final passwords = snapshot.data!;
+            return ListView.builder(
+              itemCount: passwords.length,
+              itemBuilder: (context, index) {
+                final passwordItem = passwords[index];
+                return GestureDetector(
+                  onTap: () async {
+                    final res = await Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder:
+                            (context) =>
+                                ShowPasswordScreen(passwordItem: passwordItem),
+                      ),
+                    );
+                    if (res == true) {
+                      _refreshPasswords();
+                    }
+                  },
+                  child: Card(child: ListTile(title: Text(passwordItem.site))),
+                );
+              },
+            );
+          }
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () async {
+          // Add a new password
+          final result = await Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => AddPasswordScreen()),
+          );
+          // If V
+          if (result == true) {
+            _refreshPasswords();
+          }
+        },
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+}
---
+++ lib/login_screen.dart
@@ -0,0 +1,213 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:secret_keeper/home_page.dart';
+import 'package:secret_keeper/colors.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:crypto/crypto.dart';
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({super.key});
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  bool _isMasterPasswordSet = false;
+  final _masterPasswordController = TextEditingController();
+  final _signUpPasswordController1 = TextEditingController();
+  final _signUpPasswordController2 = TextEditingController();
+
+  Future<void> setAndSaveMasterPassword() async {
+    final prefs = await SharedPreferences.getInstance();
+    final hashedMasterPassword =
+        sha256.convert(utf8.encode(_masterPasswordController.text)).toString();
+    await prefs.setBool('isMasterPasswordSet', true);
+    await prefs.setString('masterPassword', hashedMasterPassword);
+  }
+
+  Future<void> checkMasterPasswordSet() async {
+    final prefs = await SharedPreferences.getInstance();
+    return setState(() {
+      _isMasterPasswordSet = prefs.getBool('isMasterPasswordSet') ?? false;
+    });
+  }
+
+  Future<bool> checkMasterPassword() async {
+    final prefs = await SharedPreferences.getInstance();
+    final hashedMasterPassword = prefs.getString('masterPassword');
+    final hashedInputPassword =
+        sha256.convert(utf8.encode(_masterPasswordController.text)).toString();
+    return hashedMasterPassword == hashedInputPassword;
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    checkMasterPasswordSet();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      maintainBottomViewPadding: true,
+      child: Scaffold(
+        backgroundColor: context.lavender,
+        body: SizedBox(
+          width: double.infinity,
+          child: Column(
+            mainAxisSize: MainAxisSize.max,
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Image.asset(
+                    'assets/icon/logo.png',
+                    height: MediaQuery.of(context).size.height * 0.2,
+                  ),
+                  SizedBox(height: 20),
+                  Text(
+                    'Secret Keeper',
+                    style: TextStyle(
+                      fontSize: 30,
+                      fontWeight: FontWeight.bold,
+                      color: context.accent,
+                    ),
+                  ),
+                ],
+              ),
+              Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  _isMasterPasswordSet
+                      ? Column(
+                        mainAxisSize: MainAxisSize.min,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.only(left: 40),
+                            child: Text(
+                              'Enter Master Password',
+                              style: TextStyle(
+                                fontSize: 20,
+                                color: context.accent,
+                              ),
+                            ),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.fromLTRB(30, 10, 30, 10),
+                            child: TextField(
+                              controller: _masterPasswordController,
+                              obscureText: true,
+                              decoration: InputDecoration(
+                                border: OutlineInputBorder(),
+                                labelText: 'Master Password',
+                              ),
+                            ),
+                          ),
+                        ],
+                      )
+                      : Column(
+                        mainAxisSize: MainAxisSize.min,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.only(left: 40),
+                            child: Text(
+                              'Set Master Password',
+                              style: TextStyle(
+                                fontSize: 20,
+                                color: context.accent,
+                              ),
+                            ),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.fromLTRB(30, 10, 30, 10),
+                            child: TextField(
+                              controller: _signUpPasswordController1,
+                              obscureText: true,
+                              decoration: InputDecoration(
+                                fillColor: context.white,
+                                border: OutlineInputBorder(),
+                                labelText: 'Master Password',
+                              ),
+                            ),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.fromLTRB(30, 10, 30, 10),
+                            child: TextField(
+                              controller: _signUpPasswordController2,
+                              obscureText: true,
+                              decoration: InputDecoration(
+                                border: OutlineInputBorder(),
+                                labelText: 'Confirm Master Password',
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+
+                  Row(
+                    mainAxisSize: MainAxisSize.max,
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.only(right: 40),
+                        child: ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: context.accent,
+                          ),
+                          onPressed: () async {
+                            if (_isMasterPasswordSet) {
+                              bool isCorrect = await checkMasterPassword();
+                              if (isCorrect) {
+                                _navigateToHomePage(
+                                  _masterPasswordController.text,
+                                );
+                              } else {
+                                _showSnackBar('Incorrect Master Password.');
+                              }
+                            } else if (_signUpPasswordController1.text ==
+                                _signUpPasswordController2.text) {
+                              await setAndSaveMasterPassword();
+                              _navigateToHomePage(
+                                _signUpPasswordController1.text,
+                              );
+                            } else {
+                              _showSnackBar('Passwords do not match.');
+                            }
+                          },
+                          child: Text(
+                            'Enter',
+                            style: TextStyle(color: context.white),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  void _navigateToHomePage(String password) {
+    Navigator.pushReplacement(
+      context,
+      MaterialPageRoute(
+        builder: (context) => HomePage(masterPassword: password),
+      ),
+    );
+  }
+
+  void _showSnackBar(String message) {
+    ScaffoldMessenger.of(
+      context,
+    ).showSnackBar(SnackBar(content: Text(message)));
+  }
+}
